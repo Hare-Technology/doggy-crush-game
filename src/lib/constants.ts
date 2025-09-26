3831c55524e8a256f0241b4e75c7368b0783a973
Bradyn Hare <bradynhare@gmail.com>
2025-09-26 02:54:37 +0000

    can you give each emoji a diffrent background color

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -7,9 +7,9 @@ export const INITIAL_TARGET_SCORE = 1000;
 export const TILE_TYPES: TileType[] = ['paw', 'bone', 'house', 'ball', 'bowl'];
 
 export const TILE_COLORS: Record<TileType, string> = {
-    paw: 'bg-orange-400',
-    bone: 'bg-amber-200',
-    house: 'bg-red-500',
-    ball: 'bg-green-500',
-    bowl: 'bg-blue-500',
+    paw: 'bg-orange-300',
+    bone: 'bg-yellow-300',
+    house: 'bg-pink-400',
+    ball: 'bg-sky-400',
+    bowl: 'bg-lime-400',
 };
