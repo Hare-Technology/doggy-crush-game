39f41d6c59366050eff0e2911f236ada1ae3a284
Bradyn Hare <bradynhare@gmail.com>
2025-09-26 02:57:34 +0000

    yes that worked but can you do lighter colors

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -7,9 +7,9 @@ export const INITIAL_TARGET_SCORE = 1000;
 export const TILE_TYPES: TileType[] = ['paw', 'bone', 'house', 'ball', 'bowl'];
 
 export const TILE_COLORS: Record<TileType, string> = {
-    paw: '0 72% 51%', // red
-    bone: '142 76% 36%', // green
-    house: '221 83% 53%', // blue
-    ball: '45 100% 51%', // yellow
-    bowl: '283 81% 51%', // purple
+    paw: '0 72% 85%', // light red/pink
+    bone: '142 76% 85%', // light green
+    house: '221 83% 85%', // light blue
+    ball: '45 100% 85%', // light yellow
+    bowl: '283 81% 85%', // light purple
 };
