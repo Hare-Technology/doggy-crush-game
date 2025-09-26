89dfdfdd94fdb07cb97eb6597b4ce01356da2e4e
Bradyn Hare <bradynhare@gmail.com>
2025-09-26 19:57:50 +0000

    Runtime TypeError
    (0 , {imported module [project]/nodemodules/@genkit-

--- src/ai/genkit.ts
+++ src/ai/genkit.ts
@@ -6,7 +6,7 @@ export const ai = genkit({
   plugins: [
     // The googleCloud plugin must be initialized before googleAI
     // to provide credentials in a production environment.
-    googleCloud(),
+    googleCloud,
     googleAI(),
   ],
 });
