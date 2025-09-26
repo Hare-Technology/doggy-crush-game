8211e909745143acf3a51d507676b6fea6ab66c2
Bradyn Hare <bradynhare@gmail.com>
2025-09-26 02:56:39 +0000

    it didnt

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -7,9 +7,9 @@ export const INITIAL_TARGET_SCORE = 1000;
 export const TILE_TYPES: TileType[] = ['paw', 'bone', 'house', 'ball', 'bowl'];
 
 export const TILE_COLORS: Record<TileType, string> = {
-    paw: 'bg-sky-300',
-    bone: 'bg-rose-300',
-    house: 'bg-amber-300',
-    ball: 'bg-teal-300',
-    bowl: 'bg-indigo-300',
+    paw: 'bg-red-400',
+    bone: 'bg-green-400',
+    house: 'bg-blue-400',
+    ball: 'bg-yellow-400',
+    bowl: 'bg-purple-400',
 };
